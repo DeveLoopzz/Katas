cf7e54171c861420a8bdf7412b40bed4137dfba2
DeveLoopzz <develoopzz00@gmail.com>
2024-11-20 11:05:24 +0100

    uploading katas

---
+++ Kata_Chords/Chords.php
@@ -0,0 +1,82 @@
+<?php
+
+
+$acordes_principiante = [
+    'Do',
+    'Sol',
+    'Re',
+    'Mi menor',
+    'La menor',
+];
+
+// Acordes para nivel intermedio
+$acordes_intermedio = [
+    'Fa',
+    'Si menor',
+    'Re menor',
+    'Mi mayor',
+    'La mayor',
+];
+
+// Acordes para nivel avanzado
+$acordes_avanzado = [
+    'Sol7',
+    'Re7',
+    'La7',
+    'Si7',
+    'Do# menor',
+];
+
+
+$practica1 = practicarAcordesPrincipiante($acordes_principiante);
+
+echo $practica1;
+
+
+function practicarAcordesPrincipiante(array $acordes_principiante) 
+{
+    $contador = 0;
+    $contadormax = 10;
+
+    do 
+    {
+        foreach($acordes_principiante as $acorde)
+        {
+            echo $acorde . "<br>"; 
+            //sleep(3);
+        }
+        $contador++;
+    } while($contador < $contadormax);
+}
+
+function practicarAcordesIntermedio(array $acordes_intermedio)
+{
+    $contador = 0;
+    $contadormax = 10;
+
+    do 
+    {
+        foreach($acordes_intermedio as $acorde)
+        {
+            echo $acorde . "<br>"; 
+            sleep(3);
+        }
+        $contador++;
+    } while($contador < $contadormax);
+}
+
+function practicarAcordesAvanzado(array $acordes_avanzado) 
+{
+    $contador = 0;
+    $contadormax = 10;
+
+    do 
+    {
+        foreach($acordes_avanzado as $acorde)
+        {
+            echo $acorde . "<br>"; 
+            sleep(3);
+        }
+        $contador++;
+    } while($contador < $contadormax);
+}
