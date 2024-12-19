09a928a50feaf025a7bacf17e5d39af52cf08108
DeveLoopzz <develoopzz00@gmail.com>
2024-12-19 11:17:37 +0100

    version starting without * star is taking its spot

--- Kata_Arbol_Navidad/kata_arbol.php
+++ Kata_Arbol_Navidad/kata_arbol.php
@@ -4,15 +4,15 @@ $alto = intval(readline("cual es la altura de tu arbol?"));
 
 function makeTree($alto) {
     $bool = true;
-    for($i = 1; $i <= $alto; $i++) {
+    for($i = 2; $i <= $alto; $i++) {
         if($bool){
-            echo str_repeat(" ", $alto - $i);
-            echo str_repeat("*", (2 * $i - 1));
+            echo str_repeat(" ", $alto - 1);
+            echo str_repeat("*", (2 * 1 - 1));
             echo PHP_EOL;
-            echo str_repeat(" ", $alto - ($i + 2));
+            echo str_repeat(" ", $alto - (1 + 2));
             echo "* * *";
             echo PHP_EOL;
-            echo str_repeat(" ", $alto - ($i + 1));
+            echo str_repeat(" ", $alto - (1 + 1));
             echo "* *";
             echo PHP_EOL;
             $bool = false;
